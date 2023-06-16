5f6ac42cbb3f692cc57b79ef0a45d64e433e8979
Krokimonski <rodrigueznavarrod@gmail.com>
2023-06-16 16:08:49 +0200

    HOSTPORT+white

--- 313v3n-engine-main/313v3n-engine-main/src/main/java/engine_313v3n/conexion/Main.java
+++ 313v3n-engine-main/313v3n-engine-main/src/main/java/engine_313v3n/conexion/Main.java
@@ -3,13 +3,13 @@ package engine_313v3n.conexion;
 public class Main {
     static public String HTTP_PROTOCOL = "http://";
     static public String HTTPS_PROTOCOL = "https://";
-    static public String HOST = "{{host}}";
-    static public String PORT = "{{port}}";
+    static public String HOST = "frparvm97723807.corp.capgemini.com";
+    static public String PORT = "8280";
 
     public static void main(String[] args) {
         Llamadas conexion = new Llamadas();
-        String username = "admin";
-        String password = "admin";
+        String username = "313v3nWhite";
+        String password = "DBC";
         String grantType = "password";
 
         try {
